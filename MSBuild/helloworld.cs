05262105ba41bb82f06d0993987dec9d362559e4
Ida Funk <Ida.Funk@hiq.se>
2025-08-13 16:00:57 +0200

    Added MS Build folder and files

---
+++ MSBuild/helloworld.cs
@@ -0,0 +1,11 @@
+using System;
+
+class HelloWorld {
+    static void Main(){
+# if DebugConfig
+        Console.WriteLine("IN DEBUG!");
+# endif  
+
+        Console.WriteLine("Hello World!");
+    }
+}
